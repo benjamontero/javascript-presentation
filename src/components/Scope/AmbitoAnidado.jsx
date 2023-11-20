6753e702660cc109dd1d547e2143b0caf853b663
Gaston Devalis <gasdevalis@gmail.com>
2023-11-20 17:53:03 -0300

    cambios en ambibto anidado

--- src/components/Scope/AmbitoAnidado.jsx
+++ src/components/Scope/AmbitoAnidado.jsx
@@ -14,35 +14,51 @@ export const AmbitoAnidado = () => {
           <pre>
             <code>
               {
-                `//En primer lugar crearemos una funcion que en este caso decidimos llamar 'Exterior' y 
-dentro simplemente guardamos una constante.
-
-function Exterior() {
-  const x = 10;
-
-//Luego de crear esta constante, dentro de la misma funcion creamos otra funcion, 
-en este caso 'Interior'.
-
-function Interior() {
-  const y = 5;
+                `function FabricaPizzas() {
+// Función para preparar la masa de la pizza
+  const prepararMasa = () => {
   return (
-    <div>
-      <p>"La variable externa {x} junto con la variable interna {y}."</p>
+  <div>
+    <h2>Pizza de masa fina</h2>
+    <p>¡La masa está lista!</p>
+  </div>
+  );
+  };
+                
+  // Función para agregar ingredientes a la pizza
+  const agregarIngredientes = () => {
+  return (
+  <div>
+  <h3>Ingredientes</h3>
+  <ul>
+    <li>Tomate</li>
+    <li>Queso</li>
+    <li>Pepperoni</li>
+    </ul>
     </div>
   );
-}
-
-return (
+  };
+                
+  // Función principal para crear la pizza
+  const hacerPizza = () => {
+  return (
   <div>
-    Interior() // Llamada a la funcion Interior desde dentro de la funcion Exterior
+    <h1>Haciendo una deliciosa pizza</h1>
+    {prepareDough()}
+    {addIngredients()}
+    <p>¡La pizza está lista para hornear!</p>
   </div>
-)
+  );
+  };
+                
+return (
+<div className="fabrica-pizzas">
+  {hacerPizza()}
+</div>
+);
 }
-
-export default Exterior;
-
-// De esta manera se puede llamar a la funcion Exterior y a traves de ella acceder a la siguiente 
-funcion Interior junto con sus atributos y valores.` 
+                
+export default FabricaPizzas;` 
               }
             </code>            
           </pre>
@@ -53,7 +69,7 @@ funcion Interior junto con sus atributos y valores.`
         <img className="img-logo-barScript" src={chefScript} alt="chef de Barscript" />
         <h3>CHEF</h3>
         <p>
-          En este ejemplo se demuestra como las funciones pueden estar anidadas entre si, esto es util para controlar la visibilidad y el acceso a variables en diferentes niveles de anidamiento.
+          En el ejemplo anterior se ve de que trata el ambito anidado, funciones que se entrelazan las unas con las otras. En este caso vemos, la funcion 'prepararMasa' se encarga de la masa, 'agregarIngredientes' de añadir los ingredientes que el cliente desee y 'hacerPizza' es la funcion principal que llama a las otras dos para crear la pizza completa.
         </p>
       </div>
       <button>
